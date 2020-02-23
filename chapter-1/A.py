962fc103d49fae7db01268972dee40820363f0f5
Sho Taguchi <s.taguchi703@gmail.com>
2020-02-23 17:30:55 +0900

    A

--- chapter-1/A.py
+++ chapter-1/A.py
@@ -16,7 +16,21 @@ sys.stdin=f
 ##################################
 # %%
 # 以下ペースト可
-N 
+N = int(input())
 num_list = [int(item) for item in input().split()]
 
+def insertionSort(A, N):
+    print(*A)
+    for i in range(1, N):
+        v = A[i]
+        j = i -1
 
+        while j >= 0 and A[j] > v:
+            A[j+1] = A[j]
+            j -= 1
+        
+        A[j+1] = v
+
+        print(*A)
+
+insertionSort(num_list, N)
