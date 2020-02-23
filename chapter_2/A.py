03c95a8a32acddc5c7b70a0bc90d8f2c02cea138
Sho Taguchi <s.taguchi703@gmail.com>
2020-02-23 23:04:45 +0900

    Chapter2_AをAC

---
+++ chapter_2/A.py
@@ -0,0 +1,36 @@
+# %%
+# VScodeで入力をテキストから読み込んで標準入力に渡す
+import sys
+import os
+f=open(r'.\chapter_2\A_input.txt', 'r', encoding="utf-8")
+# inputをフルパスで指定
+# win10でファイルを作るとs-jisで保存されるため、読み込みをutf-8へエンコードする必要あり
+# VScodeでinput file開くとutf8になってるんだけど中身は結局s-jisになっているらしい
+sys.stdin=f
+
+#
+# 入力スニペット
+# num = int(input())
+# num_list = [int(item) for item in input().split()]
+# num_list = [input() for _ in range(3)]
+##################################
+# %%
+# 以下ペースト可
+N = int(input())
+num_list = [int(item) for item in input().split()]
+
+def bubbleSort(A, N):
+    is_flag = 1
+    cnt = 0
+
+    while is_flag:
+        is_flag = 0
+        for i in range(N-1):
+            if A[i] > A[i+1]:
+                A[i], A[i+1] = A[i+1], A[i]
+                is_flag = 1
+                cnt += 1
+    print(*A)
+    print(cnt)
+
+bubbleSort(num_list, N)
