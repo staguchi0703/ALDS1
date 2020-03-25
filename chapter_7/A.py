15c5d84912e0ec6539cf8eb23ac6f38182f8eefa
Sho Taguchi <taguchi.sho@mail.canon>
2020-03-26 08:26:32 +0900

    いけた？

--- chapter_7/A.py
+++ chapter_7/A.py
@@ -59,21 +59,17 @@ def main():
         if all(tree):
             break
 
-    print(tree)
+    for node in tree:
+        print('node {}:'.format(node[0]), end=' ')
+        print('parent =', node[1], end=', ')
+        print('depth =', node[2], end=', ')
+        if node[3] == [] and node[2] != -1:
+            print('leaf', end=', ')
+        elif node[3] and node[1] != -1:
+            print('internal node', end=', ')
+        else:
+            print('node', end=', ')
+        print(node[3])
+
 if __name__ == "__main__":
     main()
-
-
-# node 0: parent = -1, depth = 0, root, [1, 4, 10]
-# node 1: parent = 0, depth = 1, internal node, [2, 3]
-# node 2: parent = 1, depth = 2, leaf, []
-# node 3: parent = 1, depth = 2, leaf, []
-# node 4: parent = 0, depth = 1, internal node, [5, 6, 7]
-# node 5: parent = 4, depth = 2, leaf, []
-# node 6: parent = 4, depth = 2, leaf, []
-# node 7: parent = 4, depth = 2, internal node, [8, 9]
-# node 8: parent = 7, depth = 3, leaf, []
-# node 9: parent = 7, depth = 3, leaf, []
-# node 10: parent = 0, depth = 1, internal node, [11, 12]
-# node 11: parent = 10, depth = 2, leaf, []
-# node 12: parent = 10, depth = 2, leaf, []
